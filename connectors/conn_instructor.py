e695973a58d6322beb4d97045767d7f63ef442f9
Erasmo <erasmo.aln@gmail.com>
2021-07-03 10:18:06 -0300

    Create instructor connector
    This connector can:
    - Get all instructors
    - Get one specific instructor
    - Get all activities per instructor
    - Add a new instructor
    - Delete an existing instructor
    - Edit an existing instructor

---
+++ connectors/conn_instructor.py
@@ -0,0 +1,94 @@
+from classes.instructor import Instructor
+from database.run_sql import run_sql
+from classes.activity import Activity
+
+
+def get_all():
+
+    instructor_list = list()
+
+    sql = 'SELECT * FROM webuser.TB_INSTRUCTOR;'
+    results = run_sql(sql, values=None)
+
+    for row in results:
+        instructor = Instructor(
+            name=row['name'],
+            lastname=row['lastname'],
+            birth_date=row['birth_date'],
+            address=row['address'],
+            phone=row['phone'],
+            id=row['id'])
+
+        instructor_list.append(instructor)
+
+    return instructor_list
+
+
+def get_one(id):
+
+    sql = 'SELECT * FROM webuser.TB_INSTRUCTOR WHERE id = %s;'
+    value = [id]
+
+    result = run_sql(sql, value)[0]
+
+    if result is not None:
+        instructor = Instructor(
+            name=result['name'],
+            lastname=result['lastname'],
+            birth_date=result['birth_date'],
+            address=result['address'],
+            phone=result['phone'],
+            id=result['id'])
+
+    return instructor
+
+
+def get_activities(instructor_id):
+
+    activity_list = list()
+
+    sql = 'SELECT * FROM webuser.TB_ACTIVITY WHERE instructor = %s;'
+    value = [instructor_id]
+
+    results = run_sql(sql, value)
+
+    for row in results:
+        activity = Activity(
+            name=row['name'],
+            instructor=row['instructor'],
+            date=row['date'],
+            duration=row['duration'],
+            capacity=row['capacity'],
+            plan_type=row['plan_type'],
+            active=row['active'],
+            id=row['id'])
+
+        activity_list.append(activity)
+
+    return activity_list
+
+
+def new(instructor):
+
+    sql = 'INSERT INTO webuser.TB_INSTRUCTOR (name, lastname, birth_date, address, phone) VALUES (%s, %s, %s, %s, %s) RETURNING *;'
+    values = [instructor.name, instructor.lastname, instructor.birth_date, instructor.address, instructor.phone]
+
+    result = run_sql(sql, values)[0]
+
+    instructor.id = result['id']
+
+    return instructor
+
+
+def delete_one(id):
+
+    sql = 'DELETE FROM webuser.TB_INSTRUCTOR WHERE id = %s;'
+    value = [id]
+    run_sql(sql, value)
+
+
+def edit(instructor):
+
+    sql = 'UPDATE webuser.TB_INSTRUCTOR SET (name, lastname, birht_date, address, phone) = (%s, %s, %s, %s, %s) WHERE id = %s;'
+    values = [instructor.name, instructor.lastname, instructor.birth_date, instructor.address, instructor.phone, instructor.id]
+    run_sql(sql, values)
