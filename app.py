72c79eaf728b918f1e35859461ca4b966f28a247
Erasmo <erasmo.aln@gmail.com>
2021-08-16 11:30:58 -0300

    fix: add schedule blueprint

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 from controllers.controller_member import members_blueprint
 from controllers.controller_actitivities import activities_blueprint
 from controllers.controller_instructor import instructors_blueprint
+from controllers.controller_schedule import schedule_blueprint
 
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ app = Flask(__name__)
 app.register_blueprint(members_blueprint)
 app.register_blueprint(activities_blueprint)
 app.register_blueprint(instructors_blueprint)
+app.register_blueprint(schedule_blueprint)
 
 
 @app.route("/")
