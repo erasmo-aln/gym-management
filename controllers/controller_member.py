eee174b32995d0d8b8b526df55c8306d57b1e02a
Erasmo <erasmo.aln@gmail.com>
2021-08-16 11:32:18 -0300

    feat: add route to member's details page

--- controllers/controller_member.py
+++ controllers/controller_member.py
@@ -10,7 +10,19 @@ import connectors.conn_plan as connector_plan
 members_blueprint = Blueprint("members", __name__)
 
 
+# Route to index page with active members
 @members_blueprint.route("/members")
 def members_index():
     members = connector_member.get_all_active()
     return render_template("members/index.html", members=members, title="Members")
+
+
+# Route to show details
+@members_blueprint.route("/members/<id_>")
+def show_details(id_):
+    member = connector_member.get_one(id_)
+    activities = connector_member.get_activities(id_)
+    plan_type = connector_plan.get_one(member.plan_type.id_)
+    return render_template("members/details.html", member=member, plan_type=plan_type, activities=activities, title="Show Details")
+
+
