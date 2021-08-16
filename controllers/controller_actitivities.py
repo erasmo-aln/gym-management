807ba38c7e70bd1a51db0c6d23e037072dadbd1a
Erasmo <erasmo.aln@gmail.com>
2021-08-16 11:31:56 -0300

    feat: add route to activity's details page

--- controllers/controller_actitivities.py
+++ controllers/controller_actitivities.py
@@ -11,8 +11,19 @@ import connectors.conn_plan as connector_plan
 activities_blueprint = Blueprint("activities", __name__)
 
 
+# Route to index page with active activities
 @activities_blueprint.route("/activities")
 def activities_index():
     activities = connector_activity.get_all_active()
     return render_template("activities/index.html", activities=activities, title="activities")
 
+
+# Route to an specific activity
+@activities_blueprint.route("/activities/<id_>")
+def show_details(id_):
+    activity = connector_activity.get_one(id_)
+    plan_type = connector_plan.get_one(activity.plan_type.id_)
+    members = connector_activity.get_members(id_)
+    members_booked = len(members)
+    instructor = connector_instructor.get_one(activity.instructor)
+    return render_template("/activities/details.html", activity=activity, instructor=instructor, members=members, members_booked=members_booked, plan_type=plan_type, title="Activity Details")
